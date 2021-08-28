7e37967cf149111c07579ea5c227fb80e1e7291e
mor1za <moritz.weisshaupt@gmx.at>
2021-08-28 23:58:19 +0200

    added login

---
+++ login/logik.js
@@ -0,0 +1,24 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
+
+function validate()
+{
+var username=document.getElementById("username").value;
+var password=document.getElementById("password").value;
+if(username=="admin"&& password=="user")
+{
+  window.location.href="gambo-settings.github.io/login-success";
+}
+else
+{
+    alert("login failed");
+}
+
+
+
+
+}
